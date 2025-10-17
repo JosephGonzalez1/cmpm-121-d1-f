c9389b4319a8d84a4feba1c33258946737e129ff
JosephGonzalez1 <152119091+JosephGonzalez1@users.noreply.github.com>
2025-10-17 22:49:26 +0000

    Added a counter increase when button is clicked (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import fireEmoji from "./Fire.jpg";
 import "./style.css";
 
+let counter: number = 0;
+
 document.body.innerHTML = `
-  <button id="increment">
-    <img src="${fireEmoji}" class="icon" />
-  </button>
+  <p>Counter: <span id="counter">0</span></p>
+  <button id =  increment> Fire: <img src="${fireEmoji}" class="icon" /></button>
 `;
+
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter++;
+  counterElement.textContent = counter.toString();
+});
